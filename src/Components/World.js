b91d54ef8c492c4c1c57d4e7cfba9737369db52a
Thomas-Tuttle <48684599+Thomas-Tuttle@users.noreply.github.com>
2019-10-21 15:19:37 -0400

    Login and Register

---
+++ src/Components/World.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Title = styled.h1`
+
+`
+
+
+class World extends React.Component {
+
+
+
+    render() {
+        return(
+            <Title>
+                World
+            </Title>
+        )
+    }
+}
+
+export default World
