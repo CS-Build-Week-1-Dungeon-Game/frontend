7e6c1ad9b97646ed7479ff6479eb8933d2509eda
clem9281 <clem9281@gmail.com>
2019-10-24 12:40:22 -0700

    adds private route so user gets routed to loginpage if they don't have the token rather than getting stuck on the loader page

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import PrivateRoute from "./Components/PrivateRoute"
 import Login from './Components/Login';
 import Register from './Components/Register';
 import WorldPage from "./Components/WorldPage";
@@ -11,7 +12,7 @@ export default function AppRouter() {
         <Switch>
             <Route path='/register' component={Register}/>
             <Route exact path='/' component={Login}/>
-            <Route path='/world' component={WorldPage}/>
+            <PrivateRoute path='/world' component={WorldPage}/>
         </Switch>
     </div>
 }
